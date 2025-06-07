b47896fde47daa1201161813d8dbe911208b87e7
Ivan Kostrukov <kostrukov.ivan@student.ehu.lt>
2025-06-07 20:26:35 +0300

    upd

---
+++ src/components/hero.ts
@@ -0,0 +1,35 @@
+export default function hero() {
+    return `
+         <section class="hero">
+    <div class="container">
+      <div class="hero__inner">
+        <div class="hero__info">
+          <h1 class="hero__info-title">FIND <span>ANYTHING</span> THAT MATCHES YOUR STYLE</h1>
+          <p class="hero__info-text">
+            Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality
+            and cater to your sense of style.
+          </p>
+          <a class="hero__info-button" href="#">Shop Now</a>
+          <div class="hero__info-stats">
+            <div class="hero__info-stats-item">
+              <div class="hero__info-stats-item-main">200+</div>
+              <div class="hero__info-stats-item-side">International Brands</div>
+            </div>
+            <div class="hero__info-stats-item">
+              <div class="hero__info-stats-item-main">2,000+</div>
+              <div class="hero__info-stats-item-side">High-Quality Products</div>
+            </div>
+            <div class="hero__info-stats-item">
+              <div class="hero__info-stats-item-main">30,000+</div>
+              <div class="hero__info-stats-item-side">Happy Customers</div>
+            </div>
+          </div>
+        </div>
+        <div class="hero__image">
+          <img src="/assets/img/hero.jpg" alt="" class="hero__image-img">
+        </div>
+      </div>
+    </div>
+  </section>
+    `
+}
--- src/pages/homepage.ts
+++ src/pages/homepage.ts
@@ -1,9 +1,11 @@
 import header from "../components/header";
 import banner from "../components/banner";
+import hero from "../components/hero";
 
 export default function homePage() {
     return `
         ${banner()}
         ${header()}
+        ${hero()}
     `
 }
